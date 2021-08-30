43ef4f12635b55a0757b2f177211035d52ea2039
HARIHARAN B P <bphariharan1301@gmail.com>
2021-08-30 19:08:48 +0530

    OTP var comparison not working

--- verification/settings.py
+++ verification/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +128,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
--- verify_otp/views.py
+++ verify_otp/views.py
@@ -2,17 +2,15 @@ from typing import Generic
 from django.core.checks import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from verification.settings import EMAIL_HOST_USER
+import string
+from random import choice
 
 # Create your views here.
 # @csrf_protect 
 # class RegisterUserView(Generic.createview):
 def RegisterView(request):
-  first_name=""
-  last_name=""
-  # username=""
-  RegisterView.user_email=""
-  password=""
-  password2=""
   if request.method == 'POST':
     first_name = request.POST['first_name']
     last_name = request.POST['last_name']
@@ -21,13 +19,21 @@ def RegisterView(request):
     password = request.POST['password']
     password2 = request.POST['password2']
 
+    RegisterView.user_first_name = first_name
+    RegisterView.user_last_name = last_name
+    RegisterView.user_username = username
     RegisterView.user_email = email
+    RegisterView.user_password = password
+    RegisterView.user_password2 = password2
 
-    print(RegisterView.user_email)
+
+    print("register FUnction")
+
+    # print(RegisterView.user_email)
 
     if password == password2:
-      user = User.objects.create(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
-      user.save()
+      # user = User.objects.create(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
+      # user.save()
       return redirect('verify')
   else:
     return render(request, 'registration/register.html')
@@ -39,6 +45,45 @@ def logout(request):
   pass
 
 def verify(request):
-  print(RegisterView.user_email)  
+  print("Verify Function")
+  # print(RegisterView.user_email)  
+  # print(RegisterView.user_first_name)
+  # print(RegisterView.user_last_name)
+  # print(RegisterView.user_username)
+  # print(RegisterView.user_password)
   
+  # print(EMAIL_HOST_USER)
+
+  chars = string.digits
+  random_otp =  ''.join(choice(chars) for _ in range(6))
+  print("OTP: ", random_otp)
+  print("RANDOM_OTP var type before: ",type(random_otp))
+  random_otp = int(random_otp)
+  print("RANDOM_OTP var type after: ",type(random_otp))
+  # send_mail (
+  #     'OTP FOR ACCOUNT VERIFICATION',
+  #     'OPT:'+ random_otp,
+  #     EMAIL_HOST_USER,
+  #     [RegisterView.user_email],
+  #     fail_silently=False,
+  #   )
+
+  
+  if request.method == 'POST':
+    print("Inside IF")
+    otp = request.POST['otp']
+    print("opt var type",type(otp))
+
+    otp = int(otp)
+    print("opt var type after conversion",type(otp))
+
+    if otp == random_otp:
+      print("OTP ENTERED IS: ", otp)
+      # user = User.objects.create(username = RegisterView.user_username, password = RegisterView.user_password, email = RegisterView.user_email, first_name = RegisterView.user_first_name, last_name = RegisterView.user_last_name)
+      # user.save()
+      return redirect('login')
+    else:
+      messages.Error(request,'invalid otp')
+      return redirect('register')
+
   return render(request, 'registration/verify.html')
